44ec4cd53aabee8152d3973fb77070c734a30081
Florian Dehn <f.dehn@sofort.com>
2015-12-15 15:04:46 +0100

    first demo job

---
+++ jobs/first_job.groovy
@@ -0,0 +1,11 @@
+job('DSL-Tutorial-1-Test') {
+    scm {
+        git('https://github.com/katzefudder/docker-hands-on-apache.git')
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+
+    }
+}
