ed33c2408e161d3d94a242e4ce85a1226a03ec08
Florian Dehn <f.dehn@sofort.com>
2015-12-15 15:58:05 +0100

    testing

--- jobs/first_view.groovy
+++ jobs/first_view.groovy
@@ -1,24 +1,6 @@
-listView('Builds A'){
-    freeStyleJob('Seltsames Projekt') {
-        scm {
-            git('https://github.com/katzefudder/docker-hands-on-apache.git')
-        }
-        triggers {
-            scm('*/15 * * * *')
-        }
-        steps {
-
-        }
-    }
-    freeStyleJob('Meine Projekte/Ein noch seltsameres Projekt') {
-        scm {
-            git('https://github.com/katzefudder/docker-hands-on-apache.git')
-        }
-        triggers {
-            scm('*/15 * * * *')
-        }
-        steps {
-
-        }
+listView('Docker Introduction'){
+    jobs {
+        'Meine Projekte/Docker Hands on Apache',
+        'Meine Projekte/Hallo Welt echoer'
     }
 }
